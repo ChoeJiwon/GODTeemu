69ffb05e3448793db3687b1c8792ea091f14fa14
KimHyungChan <hj1003hj@naver.com>
2017-05-23 01:20:52 +0900

    By KimHyungChan

---
+++ Team_project/src/JanggiApp.java
@@ -0,0 +1,90 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ButtonGroup;
+
+public class JanggiApp extends JFrame {
+
+	private JPanel contentPane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanggiApp frame = new JanggiApp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JanggiApp() {
+		setTitle("JANGGI APP");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 594, 568);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 218, 185));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lbljanggi = new JLabel("OOP WITH JANGGI");
+		lbljanggi.setForeground(new Color(160, 82, 45));
+		lbljanggi.setFont(new Font("±¼¸²", Font.PLAIN, 50));
+		lbljanggi.setBounds(14, 12, 548, 71);
+		lbljanggi.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lbljanggi);
+		
+		JButton btnstart = new JButton("START");
+		buttonGroup.add(btnstart);
+		btnstart.setFont(new Font("±¼¸²", Font.PLAIN, 30));
+		btnstart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnstart.setForeground(new Color(221, 160, 221));
+		btnstart.setBackground(new Color(255, 218, 185));
+		btnstart.setBounds(193, 182, 179, 64);
+		contentPane.add(btnstart);
+		
+		JLabel lblintro = new JLabel("PRODUCTION BY TEEMU WARRIOUS");
+		lblintro.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblintro.setForeground(new Color(169, 169, 169));
+		lblintro.setBounds(121, 459, 326, 18);
+		contentPane.add(lblintro);
+		
+		JButton btnset = new JButton("SETTING");
+		buttonGroup.add(btnset);
+		btnset.setFont(new Font("±¼¸²", Font.PLAIN, 30));
+		btnset.setForeground(new Color(221, 160, 221));
+		btnset.setBackground(new Color(255, 218, 185));
+		btnset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SettingFrame set = new SettingFrame();
+				set.go();
+			}
+		});
+		btnset.setBounds(194, 275, 178, 64);
+		contentPane.add(btnset);
+	}
+}
---
+++ Team_project/src/SettingFrame.java
@@ -0,0 +1,106 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ButtonGroup;
+
+public class SettingFrame extends JFrame {
+
+	private JPanel Pane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	/**
+	 * Launch the application.
+	 */
+	public void go(){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SettingFrame frame2 = new SettingFrame();
+					frame2.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SettingFrame() {
+		setTitle("Setting");
+		setBounds(100, 100, 594, 568);
+		Pane = new JPanel();
+		Pane.setBackground(new Color(255, 218, 185));
+		Pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(Pane);
+		Pane.setLayout(null);
+		
+		JLabel lbljanggi = new JLabel("OOP WITH JANGGI");
+		lbljanggi.setForeground(new Color(160, 82, 45));
+		lbljanggi.setFont(new Font("±¼¸²", Font.PLAIN, 50));
+		lbljanggi.setBounds(14, 12, 548, 71);
+		lbljanggi.setHorizontalAlignment(SwingConstants.CENTER);
+		Pane.add(lbljanggi);
+		
+		JLabel lblintro = new JLabel("PRODUCTION BY TEEMU WARRIOUS");
+		lblintro.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblintro.setForeground(new Color(169, 169, 169));
+		lblintro.setBounds(121, 459, 326, 18);
+		Pane.add(lblintro);
+		
+		JButton btnset = new JButton("SETTING");
+		btnset.setFont(new Font("±¼¸²", Font.PLAIN, 30));
+		btnset.setForeground(new Color(221, 160, 221));
+		btnset.setBackground(new Color(255, 218, 185));
+		btnset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnset.setBounds(198, 314, 178, 64);
+		Pane.add(btnset);
+		
+		JLabel lblSound = new JLabel("SOUND");
+		lblSound.setFont(new Font("±¼¸²", Font.PLAIN, 30));
+		lblSound.setBounds(149, 156, 113, 47);
+		Pane.add(lblSound);
+		
+		JLabel lblColor = new JLabel("COLOR");
+		lblColor.setFont(new Font("±¼¸²", Font.PLAIN, 30));
+		lblColor.setBounds(149, 215, 113, 35);
+		Pane.add(lblColor);
+		
+		JRadioButton rdbtnOff = new JRadioButton("OFF");
+		buttonGroup.add(rdbtnOff);
+		rdbtnOff.setBackground(new Color(255, 218, 185));
+		rdbtnOff.setFont(new Font("±¼¸²", Font.PLAIN, 18));
+		rdbtnOff.setBounds(336, 156, 66, 47);
+		Pane.add(rdbtnOff);
+		
+		JRadioButton rdbtnOn = new JRadioButton("ON");
+		buttonGroup.add(rdbtnOn);
+		rdbtnOn.setBackground(new Color(255, 218, 185));
+		rdbtnOn.setFont(new Font("±¼¸²", Font.PLAIN, 18));
+		rdbtnOn.setBounds(272, 156, 58, 47);
+		Pane.add(rdbtnOn);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"GRAY", "RED", "GREEN", "BULE", "BEIGE", "YELLOW"}));
+		comboBox.setBackground(new Color(255, 255, 255));
+		comboBox.setBounds(277, 220, 123, 24);
+		Pane.add(comboBox);
+	}
+}
