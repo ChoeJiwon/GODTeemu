987e5b87a2febd4ee9e62e5482d7ca86e59df078
KimHyungChan <hj1003hj@naver.com>
2017-05-29 00:55:08 +0900

    Team Project

--- Team_project/src/JanggiApp/Sound.java
+++ Team_project/src/JanggiApp/Sound.java
@@ -1,23 +1,19 @@
 package JanggiApp;
 
 import java.io.File;
-
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Sound {
-		
-
-public void PlaySound(){
-	try{
-		Clip clip = AudioSystem.getClip();
-		clip.open(AudioSystem.getAudioInputStream(new File("move.wav")));
-		clip.start();
-		
-		Thread.sleep(clip.getMicrosecondLength()/1000);
-	}catch(Exception e){
-		System.out.println("error");
+	public void PlaySound(){
+		try{
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(new File("move.wav")));
+			clip.start();
+			
+			Thread.sleep(clip.getMicrosecondLength()/1000);
+		}catch(Exception e){
+			System.out.println("error");
+		}
 	}
-}
-
 }
