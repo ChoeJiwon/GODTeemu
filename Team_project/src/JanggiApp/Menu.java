802934e2d7a3d61b029277a68e9386c71d21797c
KimHyungChan <hj1003hj@naver.com>
2017-05-29 01:10:09 +0900

    Team Project

--- Team_project/src/JanggiApp/Menu.java
+++ Team_project/src/JanggiApp/Menu.java
@@ -62,8 +62,6 @@ public class Menu extends JFrame {
 		btnscore.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-				Player ps = new Player();
-				ps.go();
 			}
 		});
 		btnscore.setForeground(new Color(160, 82, 45));
