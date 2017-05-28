df82e7a0e5b1ba69c6acf5286cbac94d1a0dc340
Gwangs <hanson119@ajou.ac.kr>
2017-05-29 00:05:39 +0900

    sound

---
+++ Team_project/src/JanggiApp/Sound.java
@@ -0,0 +1,27 @@
+package JanggiApp;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+
+public static void main(String[] args) {
+	File sound = new File("GUNSHOT.WAV");
+	PlaySound(sound);
+}
+
+static void PlaySound(File Sound){
+	try{
+		Clip clip = AudioSystem.getClip();
+		clip.open(AudioSystem.getAudioInputStream(Sound));
+		clip.start();
+		
+		Thread.sleep(clip.getMicrosecondLength()/1000);
+	}catch(Exception e){
+		System.out.println("error");
+	}
+}
+
+}
