2d109fb8dad8a42c2bb68b902dea3d3f40b80688
Gwangs <hanson119@ajou.ac.kr>
2017-06-07 03:05:51 +0900

    같은 이름 못오게

--- Team_project/src/JanggiApp/Player.java
+++ Team_project/src/JanggiApp/Player.java
@@ -16,6 +16,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Player extends JFrame implements ActionListener {
 
@@ -23,7 +24,7 @@ public class Player extends JFrame implements ActionListener {
 
 	private JTextField p1text;
 	private JTextField p2text;
-	private JLabel lblgame,lblPlayer1,lblPlayer2,labelintro;
+	private JLabel lblgame, lblPlayer1, lblPlayer2, labelintro;
 	private JButton btnStart;
 	private Board board;
 
@@ -38,7 +39,7 @@ public class Player extends JFrame implements ActionListener {
 
 	public Player() {
 		setTitle("Player");
-		setBounds(100, 100, 688, 533);
+		setBounds(100, 100, 594, 568);
 		contentPane = new JPanel();
 		contentPane.setBackground(KoreanChess.c);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -68,58 +69,41 @@ public class Player extends JFrame implements ActionListener {
 		labelintro = new JLabel("PRODUCTION BY TEEMU WARRIORS");
 		labelintro.setForeground(new Color(169, 169, 169));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(293)
-							.addComponent(btnStart))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(216)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblPlayer1)
-									.addGap(18)
-									.addComponent(p1text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addComponent(labelintro)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblPlayer2)
-									.addGap(18)
-									.addComponent(p2text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(116)
-							.addComponent(lblgame)))
-					.addContainerGap(136, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(44)
-					.addComponent(lblgame, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
-					.addGap(93)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(p1text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblPlayer1))
-					.addGap(33)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(p2text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblPlayer2))
-					.addGap(51)
-					.addComponent(btnStart)
-					.addGap(86)
-					.addComponent(labelintro)
-					.addContainerGap())
-		);
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup()
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+						.createSequentialGroup().addGap(149)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(lblPlayer1)
+								.addComponent(lblPlayer2))
+						.addGap(37)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(p1text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(p2text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(78).addComponent(lblgame))
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(204).addComponent(btnStart))
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(176).addComponent(labelintro)))
+				.addContainerGap(138, Short.MAX_VALUE)));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup().addGap(52)
+				.addComponent(lblgame, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE).addGap(85)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblPlayer1).addComponent(
+						p1text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGap(33)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblPlayer2).addComponent(
+						p2text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGap(52).addComponent(btnStart).addGap(84).addComponent(labelintro).addGap(48)));
 		contentPane.setLayout(gl_contentPane);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		if(p1text.getText().length()!=0 && p2text.getText().length()!=0)
-		{
-			dispose();
-			board=new Board();
-			board.go();
+		if (p1text.getText().length() != 0 && p2text.getText().length() != 0) {
+			if (!(p1text.getText().equals(p2text.getText()))) {
+				dispose();
+				board = new Board();
+				board.go();
+			}
 		}
 	}
 
