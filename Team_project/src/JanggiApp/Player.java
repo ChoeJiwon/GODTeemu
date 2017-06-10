6155a984a64e7584df002261b0182c9a665198d4
Gwangs <hanson119@ajou.ac.kr>
2017-06-10 22:45:12 +0900

    aaa

--- Team_project/src/JanggiApp/Player.java
+++ Team_project/src/JanggiApp/Player.java
@@ -5,10 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
@@ -16,92 +25,133 @@ import javax.swing.ButtonGroup;
 import javax.swing.JTextField;
 
 public class Player extends JFrame {
-	private JPanel contentPane;
-	private JLabel lblintro;
-	private Board board;
-	private JTextField textPlay1;
-	private JTextField textPlay2;
+   private JPanel contentPane;
+   private JLabel lblintro;
+   private Board board;
+   JTextField textPlay1;
+   JTextField textPlay2;
 
-	/**
-	 * Launch the application.
-	 */
-	public void go() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Player frame = new Player();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+   /**
+    * Launch the application.
+    */
+   public void go() {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               Player frame = new Player();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
 
-	/**
-	 * Create the frame.
-	 */
-	public Player() {
-		setTitle("MENU");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 594, 568);
-		contentPane = new JPanel();
-		contentPane.setBackground(KoreanChess.c);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblgame = new JLabel("OOP WITH GAME");
-		lblgame.setForeground(new Color(160, 82, 45));
-		lblgame.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 50));
-		lblgame.setBounds(14, 12, 548, 71);
-		lblgame.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblgame);
-		
-		lblintro = new JLabel("PRODUCTION BY TEEMU WARRIORS");
-		lblintro.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 20));
-		lblintro.setForeground(new Color(169, 169, 169));
-		lblintro.setBounds(88, 459, 388, 18);
-		contentPane.add(lblintro);
-		
-		JButton btnstart = new JButton("START");
-		btnstart.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 30));
-		btnstart.setForeground(new Color(160, 82, 45));
-		btnstart.setBackground(KoreanChess.c);
-		btnstart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (textPlay1.getText().length() != 0 && textPlay2.getText().length() != 0) {
-					if (!(textPlay1.getText().equals(textPlay2.getText()))) {
-						dispose();
-						board = new Board();
-						board.go();
-					}
-				}
-			}
-		});
-		btnstart.setBounds(194, 321, 178, 64);
-		contentPane.add(btnstart);
-		
-		JLabel labelPlay = new JLabel("Player1 :");
-		labelPlay.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
-		labelPlay.setBounds(88, 153, 131, 28);
-		contentPane.add(labelPlay);
-		
-		textPlay1 = new JTextField();
-		textPlay1.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
-		textPlay1.setBounds(218, 150, 248, 34);
-		contentPane.add(textPlay1);
-		textPlay1.setColumns(10);
-		
-		JLabel lblPlay2 = new JLabel("Player2 :");
-		lblPlay2.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
-		lblPlay2.setBounds(88, 230, 131, 28);
-		contentPane.add(lblPlay2);
-		
-		textPlay2 = new JTextField();
-		textPlay2.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
-		textPlay2.setColumns(10);
-		textPlay2.setBounds(218, 227, 248, 34);
-		contentPane.add(textPlay2);
-	}
+   /**
+    * Create the frame.
+    */
+   public Player() {
+      setTitle("MENU");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 594, 568);
+      contentPane = new JPanel();
+      contentPane.setBackground(KoreanChess.c);
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(contentPane);
+      contentPane.setLayout(null);
+      
+      JLabel lblgame = new JLabel("OOP WITH GAME");
+      lblgame.setForeground(new Color(160, 82, 45));
+      lblgame.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 50));
+      lblgame.setBounds(14, 12, 548, 71);
+      lblgame.setHorizontalAlignment(SwingConstants.CENTER);
+      contentPane.add(lblgame);
+      
+      lblintro = new JLabel("PRODUCTION BY TEEMU WARRIORS");
+      lblintro.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 20));
+      lblintro.setForeground(new Color(169, 169, 169));
+      lblintro.setBounds(88, 459, 388, 18);
+      contentPane.add(lblintro);
+      
+      JButton btnstart = new JButton("START");
+      btnstart.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 30));
+      btnstart.setForeground(new Color(160, 82, 45));
+      btnstart.setBackground(KoreanChess.c);
+      btnstart.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            if (textPlay1.getText().length() != 0 && textPlay2.getText().length() != 0) {
+               if (!(textPlay1.getText().equals(textPlay2.getText()))) {
+                  dispose();
+                  board = new Board();
+                  board.go();
+               }
+            }
+         }
+      });
+      btnstart.setBounds(309, 318, 178, 64);
+      contentPane.add(btnstart);
+      
+      JLabel labelPlay = new JLabel("Player1 :");
+      labelPlay.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
+      labelPlay.setBounds(88, 153, 131, 28);
+      contentPane.add(labelPlay);
+      
+      textPlay1 = new JTextField();
+      textPlay1.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
+      textPlay1.setBounds(218, 150, 248, 34);
+      contentPane.add(textPlay1);
+      textPlay1.setColumns(10);
+      
+      JLabel lblPlay2 = new JLabel("Player2 :");
+      lblPlay2.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
+      lblPlay2.setBounds(88, 230, 131, 28);
+      contentPane.add(lblPlay2);
+      
+      textPlay2 = new JTextField();
+      textPlay2.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
+      textPlay2.setColumns(10);
+      textPlay2.setBounds(218, 227, 248, 34);
+      contentPane.add(textPlay2);
+      
+      JButton btnBack = new JButton("Save");
+      btnBack.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            
+            JFileChooser fc = new JFileChooser();
+            int result = fc.showSaveDialog(Player.this);
+            PlayerName pn = new PlayerName();
+            
+            if (result == JFileChooser.APPROVE_OPTION){
+               try {
+                  File f=fc.getSelectedFile();
+                  pn.setName1(textPlay1.getText());
+                  pn.setName2(textPlay2.getText());
+                  
+                  FileOutputStream fos=new FileOutputStream(f);
+                  ObjectOutputStream oos = new ObjectOutputStream(fos);
+                  oos.writeObject(pn);
+                  oos.close();
+               } catch (FileNotFoundException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               } catch (IOException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               }
+            }
+         }
+      });
+      btnBack.setBounds(88, 342, 105, 27);
+      contentPane.add(btnBack);
+      
+      JButton btnBack_1 = new JButton("Back");
+      btnBack_1.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            KoreanChess k=new KoreanChess();
+            k.go();
+         }
+      });
+      btnBack_1.setBounds(88, 398, 105, 27);
+      contentPane.add(btnBack_1);
+   }
 }
--- Team_project/src/JanggiApp/Score.java
+++ Team_project/src/JanggiApp/Score.java
@@ -20,203 +20,257 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Score extends JFrame {
 
-	private JPanel contentPane;
-	private JLabel lblgame,lblintro;
-	private JLabel lblPlayer_1;
-	private JLabel label;
-	JButton btnCheckScore;
-	JButton btnNewButton;
-	JTextArea textArea,textArea_1;
+   private JPanel contentPane;
+   private JLabel lblgame,lblintro;
+   private JLabel label;
+   JButton btnCheckScore;
+   JButton btnNewButton;
+   JTextArea textArea,textArea_1;
+   private JTextField textField;
+   private JTextField textField_1;
 
-	/**
-	 * Launch the application.
-	 */
-	public void go(){
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Score frame = new Score();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+   /**
+    * Launch the application.
+    */
+   public void go(){
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               Score frame = new Score();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
 
-	/**
-	 * Create the frame.
-	 */
-	public Score() {
-		setTitle("Score");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 594, 568);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setBackground(KoreanChess.c);
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		lblgame = new JLabel("OOP WITH GAME");
-		lblgame.setForeground(new Color(160, 82, 45));
-		lblgame.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 50));
-		lblgame.setBounds(14, 12, 548, 71);
-		lblgame.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		lblintro = new JLabel("PRODUCTION BY TEEMU WARRIOUS");
-		lblintro.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 20));
-		lblintro.setForeground(new Color(169, 169, 169));
-		lblintro.setBounds(121, 459, 326, 18);
-		
-		JLabel lblPlayer = new JLabel("Player1");
-		lblPlayer.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
-		lblPlayer.setForeground(Color.DARK_GRAY);
-		
-		lblPlayer_1 = new JLabel("Player2");
-		lblPlayer_1.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 25));
-		lblPlayer_1.setForeground(Color.DARK_GRAY);
-		
-		label = new JLabel(":");
-		label.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 23));
-		
-		btnCheckScore = new JButton("Check Score");
-		btnCheckScore.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				JFileChooser fc=new JFileChooser();
-				int result=fc.showOpenDialog(Score.this);           
-				
-				if(result==JFileChooser.APPROVE_OPTION){
-					File f=fc.getSelectedFile();
-					
-					try {
-						FileInputStream fis=new FileInputStream(f);
-						ObjectInputStream ois=new ObjectInputStream(fis);
-						
-						Object o=ois.readObject();
-						
-						if(o instanceof PlayerScore){
-							PlayerScore ps=(PlayerScore) o;
-							
-							textArea.setText(Integer.toString(ps.getPlayer1score()));
-							textArea_1.setText(Integer.toString(ps.getPlayer2score()));
-							
-						}
-						
-					} catch (FileNotFoundException fe) {
-						// TODO Auto-generated catch block
-						fe.printStackTrace();
-					} catch (IOException fe) {
-						// TODO Auto-generated catch block
-						fe.printStackTrace();
-					} catch (ClassNotFoundException fe) {
-						// TODO Auto-generated catch block
-						fe.printStackTrace();
-					}
-				}
-			}
-		});
-		
-		textArea = new JTextArea();
-		
-		textArea_1 = new JTextArea();
-		
-		btnNewButton = new JButton("Save Score");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser fc=new JFileChooser();
-				int result=fc.showSaveDialog(Score.this);
-				PlayerScore s=new PlayerScore();
-				
-				if(result==JFileChooser.APPROVE_OPTION)
-				{
-					File f=fc.getSelectedFile();
-					s.setPlayer1score(Integer.parseInt(textArea.getText()));
-					s.setPlayer2score(Integer.parseInt(textArea_1.getText()));
-					
-					try {
-						FileOutputStream fos=new FileOutputStream(f);
-						ObjectOutputStream oos = new ObjectOutputStream(fos);
-						oos.writeObject(s);
-						oos.close();
-					} catch (FileNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					
-				}
-			}
-			
-		});
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(9)
-							.addComponent(lblgame, GroupLayout.PREFERRED_SIZE, 548, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(116)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGap(31)
-									.addComponent(btnCheckScore)
-									.addGap(44)
-									.addComponent(btnNewButton))
-								.addComponent(lblintro))))
-					.addContainerGap(9, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(38)
-					.addComponent(lblPlayer, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-					.addGap(36)
-					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-					.addGap(37)
-					.addComponent(label, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-					.addGap(40)
-					.addComponent(textArea_1, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
-					.addComponent(lblPlayer_1, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
-					.addGap(41))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(7)
-					.addComponent(lblgame, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 144, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(label, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE)
-								.addComponent(textArea, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
-								.addComponent(lblPlayer, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE)
-								.addComponent(textArea_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))
-							.addGap(67)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnNewButton)
-								.addComponent(btnCheckScore))
-							.addGap(126))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblPlayer_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(214)))
-					.addComponent(lblintro, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
-		);
-		contentPane.setLayout(gl_contentPane);
-	}
+   /**
+    * Create the frame.
+    */
+   public Score() {
+      Player p=new Player();
+      setTitle("Score");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 594, 568);
+      contentPane = new JPanel();
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      contentPane.setBackground(KoreanChess.c);
+      setContentPane(contentPane);
+      contentPane.setLayout(null);
+      
+      lblgame = new JLabel("OOP WITH GAME");
+      lblgame.setForeground(new Color(160, 82, 45));
+      lblgame.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 50));
+      lblgame.setBounds(14, 12, 548, 71);
+      lblgame.setHorizontalAlignment(SwingConstants.CENTER);
+      
+      lblintro = new JLabel("PRODUCTION BY TEEMU WARRIOUS");
+      lblintro.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 20));
+      lblintro.setForeground(new Color(169, 169, 169));
+      lblintro.setBounds(121, 459, 326, 18);
+      
+      label = new JLabel(":");
+      label.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 23));
+      
+      btnCheckScore = new JButton("Check Score");
+      btnCheckScore.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent e){
+            JFileChooser fc=new JFileChooser();
+            int result=fc.showOpenDialog(Score.this);           
+            
+            if(result==JFileChooser.APPROVE_OPTION){
+               File f=fc.getSelectedFile();
+               
+               try {
+                  FileInputStream fis=new FileInputStream(f);
+                  ObjectInputStream ois=new ObjectInputStream(fis);
+                  
+                  Object o=ois.readObject();
+                  
+                  if(o instanceof PlayerScore){
+                     PlayerScore ps=(PlayerScore) o;
+                     
+                     textArea.setText(Integer.toString(ps.getPlayer1score()));
+                     textArea_1.setText(Integer.toString(ps.getPlayer2score()));
+                     
+                  }
+                  
+               } catch (FileNotFoundException fe) {
+                  // TODO Auto-generated catch block
+                  fe.printStackTrace();
+               } catch (IOException fe) {
+                  // TODO Auto-generated catch block
+                  fe.printStackTrace();
+               } catch (ClassNotFoundException fe) {
+                  // TODO Auto-generated catch block
+                  fe.printStackTrace();
+               }
+            }
+         }
+      });
+      
+      textArea = new JTextArea();
+      
+      textArea_1 = new JTextArea();
+      
+      btnNewButton = new JButton("Save Score");
+      btnNewButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            JFileChooser fc=new JFileChooser();
+            int result=fc.showSaveDialog(Score.this);
+            PlayerScore s=new PlayerScore();
+            
+            if(result==JFileChooser.APPROVE_OPTION)
+            {
+               File f=fc.getSelectedFile();
+               s.setPlayer1score(Integer.parseInt(textArea.getText()));
+               s.setPlayer2score(Integer.parseInt(textArea_1.getText()));
+               
+               try {
+                  FileOutputStream fos=new FileOutputStream(f);
+                  ObjectOutputStream oos = new ObjectOutputStream(fos);
+                  oos.writeObject(s);
+                  oos.close();
+               } catch (FileNotFoundException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               } catch (IOException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               }
+               
+            }
+         }
+         
+      });
+      
+      textField = new JTextField();
+      textField.setColumns(10);
+      
+      textField_1 = new JTextField();
+      textField_1.setColumns(10);
+      
+      JButton btnCallName = new JButton("Call Name");
+      btnCallName.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            PlayerScore ps=new PlayerScore();
+            JFileChooser fc = new JFileChooser();
+            int result = fc.showOpenDialog(Score.this);
+            
+            if (result == JFileChooser.APPROVE_OPTION) {
+               File f = fc.getSelectedFile();
 
-	
-	
-	}
+               try {
+                  FileInputStream fis = new FileInputStream(f);
+                  ObjectInputStream ois = new ObjectInputStream(fis);
 
+                  Object o = ois.readObject();
+
+                  if (o instanceof PlayerName) {
+                     PlayerName s = (PlayerName) o;
+
+                     textField.setText(s.getName1());
+                     textField_1.setText(s.getName2());
+                     
+                  }
+
+               } catch (FileNotFoundException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               } catch (IOException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               } catch (ClassNotFoundException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               }
+            }
+         }
+         
+      });
+      
+         
+      GroupLayout gl_contentPane = new GroupLayout(contentPane);
+      gl_contentPane.setHorizontalGroup(
+         gl_contentPane.createParallelGroup(Alignment.LEADING)
+            .addGroup(gl_contentPane.createSequentialGroup()
+               .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                  .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+                     .addGroup(gl_contentPane.createSequentialGroup()
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                           .addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+                              .addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                              .addGap(43)
+                              .addComponent(textArea, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
+                           .addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+                              .addComponent(btnCheckScore)
+                              .addGap(15)))
+                        .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                           .addGroup(gl_contentPane.createSequentialGroup()
+                              .addGap(37)
+                              .addComponent(label, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
+                              .addGap(40)
+                              .addComponent(textArea_1, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE))
+                           .addGroup(gl_contentPane.createSequentialGroup()
+                              .addPreferredGap(ComponentPlacement.RELATED)
+                              .addComponent(lblintro))
+                           .addGroup(gl_contentPane.createSequentialGroup()
+                              .addGap(71)
+                              .addComponent(btnNewButton))))
+                     .addGroup(gl_contentPane.createSequentialGroup()
+                        .addGap(9)
+                        .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                           .addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                           .addComponent(lblgame, GroupLayout.PREFERRED_SIZE, 548, GroupLayout.PREFERRED_SIZE))))
+                  .addGroup(gl_contentPane.createSequentialGroup()
+                     .addGap(23)
+                     .addComponent(btnCallName)))
+               .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+      );
+      gl_contentPane.setVerticalGroup(
+         gl_contentPane.createParallelGroup(Alignment.LEADING)
+            .addGroup(gl_contentPane.createSequentialGroup()
+               .addGap(7)
+               .addComponent(lblgame, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
+               .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                  .addGroup(gl_contentPane.createSequentialGroup()
+                     .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                           .addPreferredGap(ComponentPlacement.RELATED, 144, Short.MAX_VALUE)
+                           .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                              .addComponent(label, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE)
+                              .addComponent(textArea, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE)
+                              .addComponent(textArea_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))
+                           .addGap(67))
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                           .addPreferredGap(ComponentPlacement.RELATED)
+                           .addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                           .addGap(78)))
+                     .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(btnCheckScore)
+                        .addComponent(btnNewButton))
+                     .addGap(126))
+                  .addGroup(gl_contentPane.createSequentialGroup()
+                     .addGap(159)
+                     .addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                     .addPreferredGap(ComponentPlacement.RELATED, 190, Short.MAX_VALUE)
+                     .addComponent(btnCallName)
+                     .addGap(15)))
+               .addComponent(lblintro, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+      );
+      contentPane.setLayout(gl_contentPane);
+   }
+   }
