c497bb63e117f9d248dfcdcac5035ca740175621
Gwangs <hanson119@ajou.ac.kr>
2017-06-13 21:15:11 +0900

    주석

--- Team_project/src/JanggiApp/Answer.java
+++ Team_project/src/JanggiApp/Answer.java
@@ -2,6 +2,7 @@ package JanggiApp;
 
 import javax.swing.JOptionPane;
 
+// Answer about Quiz1,2,3
 public class Answer {
 	private int answer;
 
@@ -10,7 +11,7 @@ public class Answer {
 	}
 
 	public void setAnswer(int answer) {
-
+		// encapsulation with guardian and show Quiz2 about encapsulation
 		if (answer <= 0||answer>4) {
 			JOptionPane.showMessageDialog(null, "Answer must be more than 0 and smaller than 4");
 			Quiz2 q2 = new Quiz2();
